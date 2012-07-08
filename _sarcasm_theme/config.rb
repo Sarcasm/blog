2cfd2852234c33e171fa6db17d7b86c6f2792f21
Guillaume Papin <guillaume.papin@epitech.eu>
2012-07-08 16:29:46 +0200

    Update the style (WIP).

--- _sarcasm_theme/config.rb
+++ _sarcasm_theme/config.rb
@@ -4,17 +4,19 @@
 http_path = "/"
 css_dir = "../stylesheets"
 sass_dir = "sass"
-images_dir = "images"
+images_dir = "../images"
 javascripts_dir = "javascripts"
 
-# You can select your preferred output style here (can be overridden via the command line):
+# You can select your preferred output style here (can be overridden
+# via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+# To disable debugging comments that display the original location of
+# your selectors. Uncomment:
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
